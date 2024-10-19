6e337677cac1ba9d3b7627692b19c8fbb9739fdc
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-19 16:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let para=document.getElementById("status");
+let button=document.getElementById("enterBtn");
+
+button.addEventListener("click", () => {
+	para.innerHTML="<h1>Entered Metaverse</h1>";
+})
